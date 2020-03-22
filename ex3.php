98c52459a2b258de704f3b021bfb2c61776f0801
WollDarz <61837898+WollDarz@users.noreply.github.com>
2020-03-22 20:50:04 +0100

    Devoirphp1

---
+++ ex3.php
@@ -0,0 +1,39 @@
+<?php
+try{
+	$f=fopen("file.txt", "r");
+}
+catch(exeception $e){
+	echo ' l erreur est :$e';
+}
+	
+	
+	
+?>
+<html>
+<head></head>
+<body>
+<h1>centrale d'achats</h1>
+<h3>Commande clients </h3>
+<table style="width:100%">
+<link rel="stylesheet" href="./style.css">
+  <tr>
+    <th>Numéro de commande</th>
+    <th>Numéro client </th>
+    <th>Date de commande </th>
+	<th>Désignation article</th>
+    <th>Quantite (Pal)</th>
+    <th>Prix unitaire (DH) </th>
+    <th>Date de livraison </th>
+    <th>adresse client  </th>
+  </tr>
+  <?php while($s=fgets($f)){?>
+	 <tr><?php  $tab=explode( '|', $s);
+				foreach ($tab as $tab1) {
+			?>
+    <td><?php   echo $tab1;?></td> 
+   <?php
+     }
+ ?></tr><?php } ?>
+</table>
+</body>
+</html>
